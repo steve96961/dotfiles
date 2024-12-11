39b8d2d23b673137542f919527a10a417f2df638
enwe <enrico.wernado@egotec.com>
2024-12-11 14:10:39 +0100

    improve umlaut

--- .config/system_scripts/umlaute.sh
+++ .config/system_scripts/umlaute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-umlaut=$(printf  "1: Ä\n2: ä\n3: Ë\n4: ë\n5: Ï\n5: ï\n6: Ö\n7: ö\n8: Ü\n9: ü\n10: Ÿ\n11: ÿ"|fuzzel -d)
+umlaut=$(printf  "a: ä\nA: Ä\ne: ë\nE: Ë\ni: ï\nI: Ï\no: ö\nO: Ö\nu: ü\nU: Ü\ny: ÿ\nY: Ÿ\n"|fuzzel --hide-before-typing -l 1 --match-mode fuzzy -d)
 
 echo -n $umlaut|awk -F ': ' '{print $2}'|tr -d '\n'|wl-copy
