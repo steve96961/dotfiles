c33d33e082014ef6eb8ac6d5606c066e207ff611
steve96961 <58976143+steve96961@users.noreply.github.com>
2025-05-08 10:55:43 +0200

    Update packages

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo pacman -Suy --needed hyprland hyprlock tmux fish fuzzel grim slurp mako nemo swww waybar alacritty noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-font-awesome nwg-look discord k9s kubectl helm code vim docker docker-buildx chromium fastfetch pipewire iw iwd go cliphist base-devel less btop libimobiledevice usbmuxd dolphin-emu wine lutris obs-studio openvpn 7zip steam
+sudo pacman -Suy --needed hyprland hyprlock tmux fish fuzzel grim slurp mako nemo swww waybar alacritty noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-font-awesome nwg-look k9s kubectl helm code vim docker docker-buildx chromium fastfetch pipewire iw iwd go cliphist base-devel less btop libimobiledevice usbmuxd dolphin-emu wine lutris openvpn 7zip steam
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
 
-yay -S bibata-cursor-theme catppuccin-gtk-theme-mocha wlogout errands
+yay -S bibata-cursor-theme catppuccin-gtk-theme-mocha wlogout errands obs-studio-browser ultrastardx-git
 
 cp -r .config .wallpapers .vimrc ~/.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
